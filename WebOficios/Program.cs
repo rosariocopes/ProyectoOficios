79d732a69b1fefa5712e555e1796641b3d900774
copesrosario@gmail.com <copesrosario@gmail.com>
2026-05-25 22:15:08 -0300

    Creacion correcta de program

--- WebOficios/Program.cs
+++ WebOficios/Program.cs
@@ -1,17 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Oficios.Abstractions;
+using Oficios.DataAccess;
+using Oficios.Repository;
+using Oficios.Services;
+using OficiosApplications;
+using OficiosDataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbDataAccess>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+            o => o.MigrationsAssembly("Oficios.WebApi"));
+    options.UseLazyLoadingProxies();
+});
+
+builder.Services.AddScoped(typeof(IStringService), typeof(StringService));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(IApplication<>), typeof(Application<>));
+builder.Services.AddScoped(typeof(IDbContext<>), typeof(DbContext<>));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
